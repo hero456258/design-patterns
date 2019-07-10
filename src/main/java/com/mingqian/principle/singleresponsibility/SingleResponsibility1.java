633c72b5a737b504f8b4b9bef18df32d578e6d4b
wanggang@tebiemiao.com <admin123456>
2019-07-10 11:41:46 +0800

    单一设计原则

--- src/main/java/com/mingqian/principle/singleresponsibility/SingleResponsibility1.java
+++ src/main/java/com/mingqian/principle/singleresponsibility/SingleResponsibility1.java
@@ -16,9 +16,9 @@ public class SingleResponsibility1 {
 
 /**
  * 交通工具类
- * 方式1:
- *      1. 在方式1的run方法中，违反了单一职责原则
- *      2. 解决的方法非常简单，根据交通工具运行方法不同，分解成不同类即可
+ * 方案1:
+ * 1. 在方式1的run方法中，违反了单一职责原则
+ * 2. 解决的方法非常简单，根据交通工具运行方法不同，分解成不同类即可
  */
 class Vehicle {
     public void run(String vehicle) {
