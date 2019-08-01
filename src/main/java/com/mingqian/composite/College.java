c35c4110fa7d32a7028f0bc0f5b82212e07f0d79
wanggang@tebiemiao.com <admin123456>
2019-08-01 15:59:00 +0800

    组合模式

---
+++ src/main/java/com/mingqian/composite/College.java
@@ -0,0 +1,57 @@
+package com.mingqian.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mingqian on 2019/8/1.
+ */
+public class College extends OrganizationComponent {
+
+    List<OrganizationComponent> components = new ArrayList<>();
+
+    public College(String name, String desc) {
+        super(name, desc);
+    }
+
+    /**
+     * 添加方法
+     *
+     * @param organizationComponent
+     */
+    @Override
+    protected void add(OrganizationComponent organizationComponent) {
+        components.add(organizationComponent);
+    }
+
+    /**
+     * 删除
+     *
+     * @param organizationComponent
+     */
+    @Override
+    protected void remove(OrganizationComponent organizationComponent) {
+        components.remove(organizationComponent);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public String getDesc() {
+        return super.getDesc();
+    }
+
+    /**
+     * 打印方法，做成抽象的，子类都需要实现
+     */
+    @Override
+    protected void print() {
+        System.out.println("-----------" + getName() + "--------------");
+        for (OrganizationComponent component : components) {
+            component.print();
+        }
+    }
+}
